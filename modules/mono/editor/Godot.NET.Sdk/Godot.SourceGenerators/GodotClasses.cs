960ec04607f4f1e35340166ee6e38f3b5a94baab
Tobi-D7 <tobid7@outlook.de>
2021-06-08 20:10:07 +0000

    clean

--- modules/mono/editor/Godot.NET.Sdk/Godot.SourceGenerators/GodotClasses.cs
+++
@@ -1,9 +0,0 @@
-namespace Godot.SourceGenerators
-{
-    public static class GodotClasses
-    {
-        public const string Object = "Godot.Object";
-        public const string DisableGodotGeneratorsAttr = "Godot.DisableGodotGeneratorsAttribute";
-        public const string AssemblyHasScriptsAttr = "Godot.AssemblyHasScriptsAttribute";
-    }
-}
